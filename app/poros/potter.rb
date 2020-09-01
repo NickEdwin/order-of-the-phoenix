3d113d6af9a65d2fafa322e2bc15f20574315cec
Nick Edwin <nickedwin85@gmail.com>
2020-09-01 10:17:59 -0600

    Poro for house results

---
+++ app/poros/potter.rb
@@ -0,0 +1,18 @@
+class Potter
+
+  attr_reader :house_name,
+              :members
+
+  def initialize(potter_info)
+    @house_name = potter_info[:name]
+    @members = potter_info[:members]
+  end
+
+  def self.house_members(house)
+    potter_service = PotterService.new
+    potter_info = potter_service.house_members(house)
+    @houses = potter_info.map do |house|
+      Potter.new(house)
+    end
+  end
+end
